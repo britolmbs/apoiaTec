8c8c72321ac222b79ed7b1243ff1c4930e111db8
Lucas Brito <britolmbs@hotmail.com>
2025-05-30 21:42:39 -0300

    feat: crianção do componete username

---
+++ src/app/creator/[username]/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+export default async function Apoia ({
+    params,
+}: {
+    params: Promise<{username: string}>
+}) {
+    const { username } = await params;
+
+    return (
+        <div className="min-h-[calc(100vh-64px)]">
+            <div className="w-full h-64-relative bg-black">
+                <Image 
+                src={"https://github.com/britolmbs.png"}
+                alt="Banner"
+                fill
+                className="object-cover opacity-50"
+                priority
+                quality={100}
+                />
+            </div>
+            <section className="flex flex-col w-full items-center justify-center mx-auto max-w-7xl p-4 relative">
+                <div className="flex flex-col items-center">
+                    <Image
+                    src={"https://github.com/britolmbs.png"}
+                    alt="Lucas Brito"
+                    width={96}
+                    height={96}
+                    quality={100}
+                    />
+                    <h1 className="font-bold text-xl md:text-2x1 mt-20 mb-4">
+                        Brito Dev
+                    </h1>
+                </div>
+            </section>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-4 max-w-5xl">
+                <section className="hidden md:flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
+                    <p className="font-semibold text-lg">
+                        Sobre Brito Dev
+                    </p>
+                    <p className="text-gray-500 mt-2">
+                        Descrição do Lucas Dev
+                    </p>
+                </section>
+
+                <section className="bg-gray-50 rounded-md p-5 h-fit mx-2">
+                    <h3 className="font-bold text-lg">
+                        Apoie o Lucas Brito: 
+                    </h3>
+                </section>
+            </div>
+        </div>
+    )
+}

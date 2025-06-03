04573f8693439849e1cabc1f7aba64d77cb992bf
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-06-03 14:50:48 -0300

    feat: Sheet.tsx

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -26,4 +26,54 @@ function SheetPortal({...props}:
         return <SheetPrimitive.Portal data-slot="sheet-portal" {...props} />
     }
 
-    
+    function SheetOverlay({
+        className,
+        ...props
+    }: React.ComponentProps<typeof SheetPrimitive.Overlay) {
+        return (
+            <SheetPrimitive.Overlay
+            data-slot="sheet-overlay"
+            className={cn(
+             "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50",
+                className
+            )}
+            {...props}
+            />
+        )
+    }
+
+    function SheetContent({
+        className,
+        children,
+        side = 'right',
+        ...props
+    }: React.ComponentProps<typeof SheetPrimitive.Content> & {
+        side?: "top" | "right" | "bottom" | "left"
+    }) {
+        return(
+            <SheetPortal>
+            <SheetOverlay />
+            <SheetPrimitive.Content
+            data-slot="sheet-content"
+            className={cn(
+                "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out fixed z-50 flex flex-col gap-4 shadow-lg transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+                side === "right" &&
+                "data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right inset-y-0 right-0 h-fullw-3/4 boder-l sm:max-w-sm",
+                side === "left" &&
+                "data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left inset-y-0 left-0 h-full w-3/4 boder-r sm:max-w-sm",
+                side === "top" &&
+                "data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top inset-x-0 top-0 h-auto border-b",
+                side === "bottom" &&
+                "data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom inset-x-0 bottom- 0 h-auto border-t",
+                 className)}
+            {...props}
+            >
+            {children}
+            <SheetPrimitive.Close className="ring-offset-background focus:ring-ring">
+            <XIcon className="size-4" />
+            <span className="sr-only">Close</span>
+            </SheetPrimitive.Close>
+            </SheetPrimitive.Content>
+            </SheetPortal>
+        )
+    }

8a051862048e3c588732c259f763574d78997091
Lucas Brito <britolmbs@hotmail.com>
2025-06-03 21:31:44 -0300

    Finalizando Sheet

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -29,7 +29,7 @@ function SheetPortal({...props}:
     function SheetOverlay({
         className,
         ...props
-    }: React.ComponentProps<typeof SheetPrimitive.Overlay) {
+    }: React.ComponentProps<typeof SheetPrimitive.Overlay>) {
         return (
             <SheetPrimitive.Overlay
             data-slot="sheet-overlay"
@@ -76,4 +76,61 @@ function SheetPortal({...props}:
             </SheetPrimitive.Content>
             </SheetPortal>
         )
+    }
+
+    function SheetHeader({ className, ...props }: React.ComponentProps<"div">) {
+        return (
+            <div
+            data-slot="sheet-header"
+            className={cn("flex flex-col gap-1.5 p-4", className)}
+            {...props}
+            />
+        )
+    }
+
+    function SheetFooter({className, ...props}: React.ComponentProps<"div">) {
+        return (
+            <div
+            data-slot="sheet-footer"
+            className={cn("mt-auto flex flex-col gap-2 p-4", className)}
+            {...props}
+            />
+        )
+    }
+
+    function SheetTitle({
+        className,
+        ...props
+    }: React.ComponentProps<typeof SheetPrimitive.Title>) {
+        return(
+            <SheetPrimitive.Title
+            data-slot="sheet-title"
+            className={cn("text-foreground font-semibold", className)}
+            {...props}
+            />
+        )
+    }
+
+    function SheetDescription({
+        className,
+        ...props
+    }: React.ComponentProps<typeof SheetPrimitive.Description>){
+        return(
+            <SheetPrimitive.Description
+            data-slot="sheet-description"
+            className={cn("text-muted-foreground text-sm", className)}
+            {...props}
+            />
+        )
+    }
+    
+    export {
+        Sheet,
+        SheetTrigger,
+        sheetClose,
+        SheetContent,
+        SheetHeader,
+        SheetFooter,
+        SheetTitle,
+        SheetDescription,
     }

134c652cb017a1f6aef61b6653b72fa8caead1e5
Lucas Brito <britolmbs@hotmail.com>
2025-06-15 09:49:04 -0300

    feat: criando sistema de login com github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import { ArrowRight, HandCoins, Heart, Shield, Zap } from 'lucide-react';
 import { FeatureCard } from '@/components/FeatureCard';
 import { Button } from "@/components/ui/button";
+import { signIn } from '@/lib/auth';
 
 
 export default function Home() {
+
+  async function handleRegister() {
+    "use server"
+    await signIn("github", { redirectTo: "/dashboard" })
+  }
+
   return (
     <div className="flex flex-col min-h-screen bg-gradient-to-b from-white to-gray-50">
       <header className="container mx-auto py-6 px-4">
@@ -29,7 +36,7 @@ export default function Home() {
                 Receba doações diretamente dos seus seguidores através de uma pagina personalizada e elegante, sem complicações.
               </p>
               <div className="pt-4">
-                <form>
+                <form action={handleRegister}>
                   <Button
                   type='submit'
                   size='lg'

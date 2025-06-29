bb9119fc16cc0be7a38e46ca079cb309083df6ae
Lucas Brito <britolmbs@hotmail.com>
2025-06-29 12:44:20 -0300

    feat:Adiciona função getInfoUser com validação de username usando Zod

--- src/app/creator/[username]/page.tsx
+++ src/app/creator/[username]/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { getInfoUser } from "./_data-access/get-info-user";
+import { notFound } from "next/navigation";
 
 export default async function Apoia ({
     params,
@@ -6,12 +8,17 @@ export default async function Apoia ({
     params: Promise<{username: string}>
 }) {
     const { username } = await params;
+    const user = await getInfoUser({ username });
+
+    if(!user){
+      notFound()
+    }
 
     return (
         <div className="min-h-[calc(100vh-64px)]">
             <div className="w-full h-64 relative bg-black">
                 <Image 
-                src={"https://github.com/britolmbs.png"}
+                src={user.image ?? "https://github.com/britolmbs.png"}
                 alt="Banner"
                 fill
                 className="object-cover opacity-50"
@@ -22,7 +29,7 @@ export default async function Apoia ({
             <section className="flex flex-col w-full items-center justify-center mx-auto max-w-7xl p-4 relative">
                 <div className="flex flex-col items-center">
                     <Image
-                    src={"https://github.com/britolmbs.png"}
+                    src={user.image ?? "https://github.com/britolmbs.png"}
                     className="w-36 h-36 rounded-xl bg-gray-50 hover:shadow-lg duration-300 select-none text-zinc-900 text-3xl flex items-center justify-center object-cover absolute -top-16 border-4 border-white"
                     alt="Lucas Brito"
                     width={96}
@@ -30,24 +37,24 @@ export default async function Apoia ({
                     quality={100}
                     />
                     <h1 className="font-bold text-xl md:text-2x1 mt-20 mb-4">
-                        Brito Dev
+                       {user.name ?? "Sem nome" }
                     </h1>
                 </div>
             </section>
 
             <div className="grid grid-cols-1 md:grid-cols-2 w-full mx-auto gap-4 max-w-5xl">
-                <section className="hidden md:flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
+                <section className="flex flex-col bg-gray-50 p-5 rounded-md h-fit mx-2">
                     <p className="font-semibold text-lg">
-                        Sobre Brito Dev
+                        Sobre {user.username ?? ""}
                     </p>
                     <p className="text-gray-500 mt-2">
-                        Descrição do Lucas Dev
+                       {user.bio ?? "Sem Biografia"}
                     </p>
                 </section>
 
                 <section className="bg-gray-50 rounded-md p-5 h-fit mx-2">
                     <h3 className="font-semibold text-lg">
-                        Apoie o Lucas Brito: 
+                     {user.name ? `Doar para ${user.name}` : "Apoiar Criador"}
                     </h3>
                 </section>
             </div>
